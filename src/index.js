48488d71880033fe6a7655158d9a7992c7bc1a0f
Geovani de Souza <geovanisouza92@gmail.com>
2016-12-09 00:41:43 -0200

    Comentários da atividade

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+// TODO: Importe o Provider do react-redux, a store e
+// a ação que carrega os produtos
 import App from './containers/App'
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
 
+// TODO: Despache a primeira ação para a store
+
+// TODO: Envolva o container App no Provider passando
+// a store
 ReactDOM.render(
   <App />,
   document.getElementById('root')
