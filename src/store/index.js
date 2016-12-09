49b445cc11c53ca48a78868e345f2abe148066b3
Geovani de Souza <geovanisouza92@gmail.com>
2016-12-09 00:43:40 -0200

    Configura integração com Redux DevTools

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
 import {createStore, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import createLogger from 'redux-logger'
+import {composeWithDevTools} from 'redux-devtools-extension'
 import reducer from './reducer'
 
-const store = createStore(reducer,
+const finalCreateStore = composeWithDevTools(
   applyMiddleware(thunk, createLogger())
-)
+)(createStore)
+
+const store = finalCreateStore(reducer)
 
 export default store
